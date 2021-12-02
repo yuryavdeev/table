0832728768edcf72303a46ca0099204eee090cb0
Yury Avdeev <avdeiev@gmail.com>
2021-12-02 13:36:45 +0300

    Updates

---
+++ precache-manifest.006a482c38c160438102883140d8bdb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28a62bbd48abda91427",
+    "url": "/table/css/app.6b51fc4a.css"
+  },
+  {
+    "revision": "935e7af77897ce5da3ea",
+    "url": "/table/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "fb2e384cdc2bfb76fc16c5f9e18ca2bd",
+    "url": "/table/index.html"
+  },
+  {
+    "revision": "a28a62bbd48abda91427",
+    "url": "/table/js/app.c865da0e.js"
+  },
+  {
+    "revision": "935e7af77897ce5da3ea",
+    "url": "/table/js/chunk-vendors.cab83ac8.js"
+  },
+  {
+    "revision": "608b5f47ed78c9c5eada841b4025f4aa",
+    "url": "/table/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/table/robots.txt"
+  }
+]);
